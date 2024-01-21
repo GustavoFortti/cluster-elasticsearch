6642444e33bf5b61676c0c4623b7485dffcc4ff3
Gustavo Fortti <gustavofortti@gmail.com>
2024-01-21 19:17:08 -0300

    novas configuracoes para o elastic

--- elasticsearch/cluster/update.sh
+++ elasticsearch/cluster/update.sh
@@ -1,5 +1,3 @@
 docker cp ./config/elasticsearch.yml elasticsearch-node:/usr/share/elasticsearch/config/elasticsearch.yml
 
-docker cp ./config/http_ca.crt elasticsearch-node:/usr/share/elasticsearch/config/certs/http_ca.crt
-
 docker restart elasticsearch-node
