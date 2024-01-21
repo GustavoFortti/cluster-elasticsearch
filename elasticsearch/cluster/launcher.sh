abf9a728523fcd6869fcadda91b179d3c30c5850
Gustavo Fortti <gustavofortti@gmail.com>
2024-01-21 18:52:54 -0300

    novas configuracoes para o elastic

--- elasticsearch/cluster/launcher.sh
+++ elasticsearch/cluster/launcher.sh
@@ -4,6 +4,8 @@ export NGROK_TOKEN="2b6H4dh1PBgx1o1l1Xl0W9cZhl5_2AvNdHVqTaeurHPrcSiMM"
 
 function elasticsearch() {
     echo "Elasticsearch LOADING..."
+    sudo sysctl -w vm.max_map_count=262144
+
     docker-compose up -d
 
     status=$?
@@ -16,10 +18,8 @@ function elasticsearch() {
 function ngrok() {
     echo "ngrok LOADING..."
     docker-compose -f docker-compose-ngrok.yml up -d
-
     # curl http://localhost:5000/api/tunnels
-    
 }
 
 elasticsearch
-ngrok
+# ngrok
