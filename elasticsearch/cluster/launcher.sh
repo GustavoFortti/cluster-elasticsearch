43a4dc564561bed84b633ea85b9959a1e2159c6f
Gustavo Fortti <gustavofortti@gmail.com>
2024-01-25 21:53:12 -0300

    novas configuracoes para o elastic

--- elasticsearch/cluster/launcher.sh
+++ elasticsearch/cluster/launcher.sh
@@ -4,18 +4,11 @@ source ./scripts/utils.sh
 
 export ES_VERSION=8.10.4
 message "ES_VERSION=$ES_VERSION"
-message "ES_CLUSTER=$ES_CLUSTER"
-message "ES_NODE=$ES_NODE"
-message "ES_SEEDS=$ES_SEEDS"
 
-# bash ./setup/setup.sh
-cp -r ./setup/certs ./
-unzip -o ./certs/ca.zip -d ./certs/
-unzip -o ./certs/certs.zip -d ./certs/
-chmod -R 777 ./certs/
-
-# docker-compose down
-sudo sysctl -w vm.max_map_count=262144
-docker-compose up -d
-docker logs es-node-1 -f
-# docker-compose down 
+function gen_certs() {
+    bash ./setup/setup.sh
+    cp -r ./setup/certs ./
+    unzip -o ./certs/ca.zip -d ./certs/
+    unzip -o ./certs/certs.zip -d ./certs/
+    chmod -R 777 ./certs/
+}
